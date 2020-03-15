fb575ad50c1dd52018bef612694a232364e93495
Wietse Wind <mail@wietse.com>
2020-03-15 23:12:59 +0100

    Add dynamic config loading & config caching

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,43 +7,60 @@ const log = Debug('app')
 
 // TODO: pm2 process exit restarts..? Kill in some kind of precheck
 
+/**
+ * Please note: the config will be auto-reloaded every X seconds
+ * See below (cacheValidSeconds)
+ */
+
+let config: any
+let configFetchMoment: number
+
+const getCacheLimit = () => {
+  const cacheValidSeconds = 60 * 5 // 5 minutes
+  return (Math.round(+new Date() / 1000) - cacheValidSeconds)
+}
+
 const get = () => {
-  let config
+  if (typeof config === 'undefined' || configFetchMoment < getCacheLimit()) {
+    log('Getting config from FS...')
 
-  log('Getting...')
-
-  try {
-    config = fs.readFileSync(path.resolve(__dirname, '..', '..', 'config.json'))
-    log('Got config from [ config.json ]')
-  } catch (e) {
-    log(`Config not found (config.json, (${e.message})), trying default config...`)
-  }
-
-  if (typeof config === 'undefined') {
     try {
-      config = fs.readFileSync(path.resolve(__dirname, '..', '..', 'config.default.json'))
-      log('Got config from [ config.default.json ]')
+      config = fs.readFileSync(path.resolve(__dirname, '..', '..', 'config.json'))
+      log('Got config from [ config.json ]')
+      configFetchMoment = Math.round(+new Date() / 1000)
     } catch (e) {
-      log('Cannot read default config either:', e.message)
+      log(`Config not found (config.json, (${e.message})), trying default config...`)
+    }
+
+    if (typeof config === 'undefined') {
+      try {
+        config = fs.readFileSync(path.resolve(__dirname, '..', '..', 'config.default.json'))
+        log('Got config from [ config.default.json ]')
+        configFetchMoment = Math.round(+new Date() / 1000)
+      } catch (e) {
+        log('Cannot read default config either:', e.message)
+        process.exit(1)
+      }
+    }
+
+    try {
+      config = JSON.parse(typeof config === 'undefined' ? '' : config.toString())
+      if (typeof config.uplinks === 'undefined') {
+        config.uplinks = [
+          {
+            type: 'fallback',
+            endpoint: 'wss://s2.ripple.com/#fallback'
+          }
+        ]
+      }
+      config.uplinks.push({type: 'basic', endpoint: 'wss://s2.ripple.com/#basic', healthy: false})
+      config.uplinks.push({type: 'priority', endpoint: 'wss://s2.ripple.com/#priority', healthy: false})
+    } catch (e) {
+      log('Cannot read JSON from config:', e.message)
       process.exit(1)
     }
-  }
-
-  try {
-    config = JSON.parse(typeof config === 'undefined' ? '' : config.toString())
-    if (typeof config.uplinks === 'undefined') {
-      config.uplinks = [
-        {
-          type: 'fallback',
-          endpoint: 'wss://s2.ripple.com/#fallback'
-        }
-      ]
-    }
-    config.uplinks.push({type: 'basic', endpoint: 'wss://s2.ripple.com/#basic', healthy: false})
-    config.uplinks.push({type: 'priority', endpoint: 'wss://s2.ripple.com/#priority', healthy: false})
-  } catch (e) {
-    log('Cannot read JSON from config:', e.message)
-    process.exit(1)
+  } else {
+    // log('Config from cache')
   }
 
   return config
